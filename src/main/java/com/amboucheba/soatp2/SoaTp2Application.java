82868d23414635719d3a3c3dfcb16b830827594e
Amine Bouchebaba <mac@MacBook-Pro-de-MAC.local>
2020-11-12 08:30:03 +0100

    initial commit

---
+++ src/main/java/com/amboucheba/soatp2/SoaTp2Application.java
@@ -0,0 +1,21 @@
+package com.amboucheba.soatp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SoaTp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaTp2Application.class, args);
+	}
+
+	@GetMapping(produces = "text/html")
+	public String home(){
+		return "<html><body><p> Home page </p></body></html>";
+	}
+
+}
---
+++ src/test/java/com/amboucheba/soatp2/SoaTp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amboucheba.soatp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaTp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
