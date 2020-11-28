9d95b98781aa1b0d5e744294fcfb46b0a9f444aa
Amine Bouchebaba <ABouchebaba@outlook.com>
2020-11-28 14:22:33 +0100

    adjusted create sql script

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,50 +1,51 @@
-CREATE TABLE if not EXISTS User(
-  id AUTOINCREMENT,
-  username VARCHAR(255) not null UNIQUE,
-  password varchar(255) not null,
+ CREATE TABLE if not EXISTS users(
+   id  integer GENERATED ALWAYS AS IDENTITY,
+   username VARCHAR(255) not null UNIQUE,
+   password varchar(255) not null,
 
-  PRIMARY KEY (id),
-  );
+   PRIMARY KEY (id)
+   );
 
-CREATE TABLE if not EXISTS SerieTemporelle(
-  id AUTOINCREMENT,
-  titre VARCHAR(255) not null,
-  description varchar(255) not null,
-  owner Varchar(255),
+ CREATE TABLE if not EXISTS SeriesTemporelles(
+   id integer GENERATED ALWAYS AS IDENTITY,
+   titre VARCHAR(255) not null,
+   description varchar(255) not null,
+   owner int,
 
-  PRIMARY KEY (id),
-  FOREIGN KEY (owner) REFERENCES User(id)
-  );
+   PRIMARY KEY (id),
+   FOREIGN KEY (owner) REFERENCES users(id)
+   );
 
-CREATE TABLE if not EXISTS Partage(
-  id AUTOINCREMENT,
-  user VARCHAR(255),
-  id_SerieTemporelle varchar(255),
-  type char(1) not null,
 
-  PRIMARY KEY (id),
-  FOREIGN KEY (user) REFERENCES User(id),
-  FOREIGN KEY (id_SerieTemporelle) REFERENCES SerieTemporelle(id)
-  );
+ CREATE TABLE if not EXISTS Partages(
+   id integer GENERATED ALWAYS AS IDENTITY,
+   id_user int,
+   id_SerieTemporelle int,
+   type char(1) not null,
 
-CREATE TABLE if not EXISTS Evenement(
-  id AUTOINCREMENT,
-  id_SerieTemporelle int,
-  date_Evenement DATE not null,
-  valeur float not null,
-  commentaire VARCHAR(255),
+   PRIMARY KEY (id),
+   FOREIGN KEY (id_user) REFERENCES users(id),
+   FOREIGN KEY (id_SerieTemporelle) REFERENCES SeriesTemporelles(id)
+   );
 
-  PRIMARY KEY (id),
-  FOREIGN KEY (id_SerieTemporelle) REFERENCES SerieTemporelle(id)
-  );
 
-CREATE TABLE if not EXISTS Tag(
-  id AUTOINCREMENT,
+ CREATE TABLE if not EXISTS Evenements(
+   id integer GENERATED ALWAYS AS IDENTITY,
+   id_SerieTemporelle int,
+   date_Evenement DATE not null,
+   valeur float not null,
+   commentaire VARCHAR(255),
+
+   PRIMARY KEY (id),
+   FOREIGN KEY (id_SerieTemporelle) REFERENCES SeriesTemporelles(id)
+   );
+  
+  CREATE TABLE if not EXISTS Tags(
+  id integer GENERATED ALWAYS AS IDENTITY,
   name varchar(255) UNIQUE not null,
   id_Evenement int,
 
-  PRIMARY KEY (name),
-  FOREIGN KEY (id_Evenement) REFERENCES Evenement(id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_Evenement) REFERENCES Evenements(id)
   );
-
-
+  
