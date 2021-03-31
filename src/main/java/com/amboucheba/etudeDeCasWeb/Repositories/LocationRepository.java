1b076c180d62c6cd28f3179eb690facc7f6834f1
Kamar MEDDAH <kamar.meddah@gmail.com>
2021-03-31 03:51:41 +0200

    feat(jwt): extend jwt token expiration time

--- src/main/java/com/amboucheba/etudeDeCasWeb/Repositories/LocationRepository.java
+++ src/main/java/com/amboucheba/etudeDeCasWeb/Repositories/LocationRepository.java
@@ -4,6 +4,7 @@ import com.amboucheba.etudeDeCasWeb.Models.Entities.Location;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
+
 public interface LocationRepository extends CrudRepository<Location, Long> {
 	List<Location> findByOwnerId(long ownerId);
 	Optional<Location> findByTitleAndOwnerId(String title, long ownerId);
